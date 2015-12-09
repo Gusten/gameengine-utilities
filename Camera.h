783005ab5715ccc440dd7df30d070be1a50c51d0
Gusten <johan.gusten@gmail.com>
2015-12-09 01:47:40 +0100

    Added first skeleton of Camera and Vector class

---
+++ Camera.h
@@ -0,0 +1,18 @@
+#ifndef __CAMERA_H_INCLUDED__
+#define __CAMERA_H_INCLUDED__
+
+#include "<math.h>"
+
+class Camera 
+{
+public:
+
+	// Camera is represented using the 3 vectors position, forward and up.
+	//float3 position;
+	//float3 forward;
+	//float3 up;
+
+
+};
+
+#endif 
---
+++ Float3.h
@@ -0,0 +1,15 @@
+#ifndef __FLOAT3_H_INCLUDED__
+#define __FLOAT3_H_INCLUDED__
+
+#include "<math.h>"
+
+class Float3
+{
+public:
+	float x;
+	float y;
+	float z;
+
+};
+
+#endif 
